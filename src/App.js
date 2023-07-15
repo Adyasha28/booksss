021e7ad2e64ce06f5cf16d71aa7f64ed25b8895c
gyanabritish <gyannayak396@gmail.com>
2023-07-15 11:41:37 +0530

    updating the code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route,Routes} from 'react-router-dom'
+import Navbar from './Components/Nabvar/Navbar';
+import LandingPage from './Components/LandingPage/Landingpage';
+import Search from './Components/SearchPage/SearchPage';
+import Login from './Components/loginPage/login';
+import Wishlist from './Components/Wishlist/Wishlist';
+function App() {
+  return (
+    <div className="App">
+     
+      <Navbar />
+      <Routes>
+          <Route exact path='/' element={<LandingPage />} />
+          <Route exact path='/Search' element={<Search />} /> 
+          <Route exact path='/wishlist' element={<Wishlist />} /> 
+          <Route exact path='/Login' element={<Login />} /> 
+          
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nabvar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import "./navbar.css"
+import { BsFillBookFill } from 'react-icons/bs';
+import { useState } from "react";
+import {AiOutlineBars} from "react-icons/ai";
+
+
+
+function Navbar(){
+
+  const[color,setColor] = useState(false)
+  const changeColor = () =>{
+      if(window.scrollY >= 100)
+        {setColor(true);}
+        else {setColor(false);}
+  };
+    window.addEventListener("scroll",changeColor);
+    return(
+    <div className="menu">
+      
+        <div className="Navbar">
+        
+        <div className="">
+           <nav className={color?'main2':'main1'}>
+             <div className="link"> 
+                   <div className="port"  >
+                       <Link to="/" >
+                       <h2 className="heading"><BsFillBookFill size={35} className={color?"log1":"log"} /></h2></Link> 
+                   
+               <ul>
+                 <li>
+                 <Link to='/' className={color?"navigation1":"navigation"}>Home </Link> 
+                 </li>
+                 <li>
+                 <Link to='./search' className={color?"navigation1":"navigation"}>Search </Link>
+                 </li>
+                 <li>
+                 <Link to='./Wishlist' className={color?"navigation1":"navigation"}>Favourites</Link>
+                 </li>
+                 <li>
+                 <Link to='./login' className={color?"navigation1":"navigation"}>Login </Link>
+                 </li>
+               </ul>
+               </div>
+             </ div>
+             </ nav>
+                  
+             </div>
+             <div className="position"> <AiOutlineBars size={50} className="hamburger"/></div>
+           </div>
+          
+         
+    </div>
+     
+          
+
+         
+        
+    
+
+    );
+}
+
+export default Navbar;
